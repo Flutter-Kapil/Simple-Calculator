554a636ce3e6a1b03d6a0261ae2aefb74cea1a94
kapil jhajhria <jhajhria44@gmail.com>
2019-10-05 11:19:08 +0530

    Almost done

---
+++ lib/calcLogic.dart
@@ -0,0 +1,37 @@
+import 'package:petitparser/petitparser.dart';
+
+caLogic() {
+  final builder = ExpressionBuilder();
+
+  builder.group()
+    ..primitive(digit()
+        .plus()
+        .seq(char('.').seq(digit().plus()).optional())
+        .flatten()
+        .trim()
+        .map((a) => num.tryParse(a)))
+    ..wrapper(char('(').trim(), char(')').trim(), (l, a, r) => a);
+
+  // negation is a prefix operator
+  builder.group()..prefix(char('-').trim(), (op, a) => -a);
+
+// power is right-associative
+  var math;
+  builder.group()..right(char('^').trim(), (a, op, b) => math.pow(a, b));
+
+// multiplication and addition are left-associative
+  builder.group()
+    ..left(char('*').trim(), (a, op, b) => a * b)
+    ..left(char('/').trim(), (a, op, b) => a / b);
+  builder.group()
+    ..left(char('+').trim(), (a, op, b) => a + b)
+    ..left(char('-').trim(), (a, op, b) => a - b);
+
+  final parser = builder.build().end();
+  String fromButtons =
+      ""; // add all the strings as soon as user taps on = and pass it
+  //to below function for solution
+  var finalOutput = parser.parse('24*(2)');
+  print(finalOutput);
+  // above example from https://github.com/petitparser/dart-petitparser#writing-a-more-complicated-grammar
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:petitparser/petitparser.dart';
 
-void main() => runApp(MaterialApp(
-      home: CalculatorApp(),
-    ));
+void main() => runApp(
+      MaterialApp(
+        home: CalculatorApp(),
+      ),
+    );
+var initialDisplay = "0";
+var initialOutput = "";
+var finalDisplay = "";
 Widget _calBtn(String btnName) {
   return Expanded(
     child: Container(
@@ -17,6 +23,9 @@ Widget _calBtn(String btnName) {
           style: TextStyle(fontSize: 25),
         ),
         color: Colors.yellow,
+        onPressed: () {
+          finalDisplay = finalDisplay + btnName;
+        },
       ),
     ),
   );
@@ -45,7 +54,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
                 padding: EdgeInsets.all(10.0),
                 color: Colors.black26,
                 child: Text(
-                  "Output",
+                  "$initialDisplay",
                   style: TextStyle(fontSize: 40),
                 ),
                 alignment: Alignment.bottomRight,
@@ -56,10 +65,22 @@ class _CalculatorAppState extends State<CalculatorApp> {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
+                  _calBtn('7'),
+                  _calBtn('8'),
+                  _calBtn('9'),
+                  _calBtn('/'),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  _calBtn('4'),
+                  _calBtn('5'),
+                  _calBtn('6'),
+                  _calBtn('*'),
                 ],
               ),
             ),
@@ -69,9 +90,9 @@ class _CalculatorAppState extends State<CalculatorApp> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
                   _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
+                  _calBtn('2'),
+                  _calBtn('3'),
+                  _calBtn('-'),
                 ],
               ),
             ),
@@ -80,34 +101,75 @@ class _CalculatorAppState extends State<CalculatorApp> {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
+                  Expanded(
+                    child: FlatButton(
+                      padding: EdgeInsets.all(2.0),
+                      splashColor: Colors.blueGrey,
+                      child: Text(
+                        'Clear',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      color: Colors.yellow,
+                      onPressed: () {
+                        setState(() {
+                          initialDisplay = "0";
+                          finalDisplay = "";
+                        });
+                      },
+                    ),
+                  ),
+                  _calBtn('0'),
+                  _calBtn('.'),
+                  _calBtn('+'),
                 ],
               ),
             ),
             Expanded(
               flex: 1,
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
-                  _calBtn('1'),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4.0),
+                      color: Colors.blue.shade400,
+                      child: FlatButton(
+                        padding: EdgeInsets.all(2.0),
+                        splashColor: Colors.blueGrey,
+                        child: Text(
+                          'Clear',
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        color: Colors.yellow,
+                        onPressed: () {
+                          setState(() {
+                            initialDisplay = "0";
+                            finalDisplay = "";
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4.0),
+                      color: Colors.blue.shade400,
+                      child: FlatButton(
+                        padding: EdgeInsets.all(2.0),
+                        splashColor: Colors.blueGrey,
+                        child: Text(
+                          '=',
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        color: Colors.yellow,
+                        onPressed: () {
+                          setState(() {
+                            initialDisplay = finalDisplay;
+                          });
+                          initialDisplay = finalDisplay;
+                        },
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -116,4 +178,40 @@ class _CalculatorAppState extends State<CalculatorApp> {
       ),
     );
   }
+}
+
+caLogic() {
+  final builderC = ExpressionBuilder();
+
+  builderC.group()
+    ..primitive(digit()
+        .plus()
+        .seq(char('.').seq(digit().plus()).optional())
+        .flatten()
+        .trim()
+        .map((a) => num.tryParse(a)))
+    ..wrapper(char('(').trim(), char(')').trim(), (l, a, r) => a);
+
+  // negation is a prefix operator
+  builderC.group()..prefix(char('-').trim(), (op, a) => -a);
+
+// power is right-associative
+  var math;
+  builderC.group()..right(char('^').trim(), (a, op, b) => math.pow(a, b));
+
+// multiplication and addition are left-associative
+  builderC.group()
+    ..left(char('*').trim(), (a, op, b) => a * b)
+    ..left(char('/').trim(), (a, op, b) => a / b);
+  builderC.group()
+    ..left(char('+').trim(), (a, op, b) => a + b)
+    ..left(char('-').trim(), (a, op, b) => a - b);
+
+  final parser = builderC.build().end();
+//  String fromButtons =
+//      ""; // add all the strings as soon as user taps on = and pass it
+  //to below function for solution
+  var finalOutput = parser.parse('$finalDisplay');
+  print(finalOutput);
+  // above example from https://github.com/petitparser/dart-petitparser#writing-a-more-complicated-grammar
 }
